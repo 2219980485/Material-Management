626b4ceba072bcfc61ca30d89be06dc4cc310ebf
2219980485 <89306123+2219980485@users.noreply.github.com>
2021-12-05 10:42:11 +0530

    Add files via upload

---
+++ Pool.py
@@ -0,0 +1,7 @@
+import pymysql as mysql
+def ConnectionPool():
+    #db = mysql.connect(host='localhost', port=3306, user="root", password="123", db="mm")
+
+    db=mysql.connect(host='campusshala.com',port=3306,user="campussh_mm",password="sandeep123@",db="campussh_mm")
+    cmd=db.cursor()
+    return (db,cmd)
